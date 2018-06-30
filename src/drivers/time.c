1321ac5f4f7f6cca5bf03cda0161f1eb31ca0846
Will Povell <william_povell@brown.edu>
2018-06-30 16:15:21 -0700

    Cleaned up time

--- src/drivers/time.c
+++ src/drivers/time.c
@@ -1,20 +1,22 @@
 #include "drivers/base.h"
 #include "drivers/time.h"
 
-#define TIMER_LOW ((uint32_t *)(PBASE + 0x3004))
-#define TIMER_HIGH ((uint32_t *)(PBASE + 0x3008))
+/* All the magic here is described in Section 12 */
+
+#define TIMER_LOW (PBASE + 0x3004)
+#define TIMER_HIGH (PBASE + 0x3008)
 
 /*
 Returns systime in micro seconds. No source for that, just basing off of
 other peoples code comments...
 */
 uint64_t get_systime() {
-  uint32_t hi = *TIMER_HIGH;
-  uint32_t lo = *TIMER_LOW;
+  uint32_t hi = get32(TIMER_HIGH);
+  uint32_t lo = get32(TIMER_LOW);
   // Redo if high just changed during read
-  if (hi != *TIMER_HIGH) {
-    hi = *TIMER_HIGH;
-    lo = *TIMER_LOW;
+  if (hi != get32(TIMER_HIGH)) {
+    hi = get32(TIMER_HIGH);
+    lo = get32(TIMER_LOW);
   }
 
   return ((uint64_t)hi << 32) | lo;
@@ -35,6 +37,5 @@ void wait_usec(uint64_t time) {
     ;
 }
 
-void wait_msec(uint32_t time) { wait_usec(time * 1000); }
-
-void wait_sec(uint32_t time) { wait_usec(time * 1000000); }
+inline void wait_msec(uint32_t time) { wait_usec(time * 1000); }
+inline void wait_sec(uint32_t time) { wait_usec(time * 1000000); }
