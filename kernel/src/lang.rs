1034b6b6641ce1b58bfab7f2f0c2df10243213ac
Will Povell <william_povell@brown.edu>
2018-10-23 23:47:11 -0400

    Added stub eh_personality

--- kernel/src/lang.rs
+++ kernel/src/lang.rs
@@ -6,3 +6,8 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+/// Used for stack unwinding.
+/// Since our panic behavior is abort this will never happen, but quiets some warnings
+#[lang = "eh_personality"]
+pub extern "C" fn eh_personality() {}
